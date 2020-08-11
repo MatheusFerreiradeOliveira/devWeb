c07b8ddd21d180ccee19db4c2d50a9fac237108f
matheusferreiradeoliveira <matheusferreiradeoliveira21@gmail.com>
2020-08-11 17:30:50 -0300

    tocando musica

--- playmusic.js
+++ playmusic.js
@@ -1,5 +1,12 @@
 var music=["song01", "song02", "song03", "song04"];
-var player;
+var player = {
+	name: '',
+	age: '',
+	tocar(str) {
+		return console.log(`Tocando ${str}`)
+	}
+
+}
 function play(){
 	for(m in music){
 		console.log(m);
