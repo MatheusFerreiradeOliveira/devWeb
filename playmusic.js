1b96250112c1f40c566f23cfac7b60641f7d993b
AbdalaHelen <helenabdala14@gmail.com>
2020-08-12 09:46:57 -0300

    function pause

--- playmusic.js
+++ playmusic.js
@@ -1,31 +1,32 @@
-var music=["song01", "song02", "song03", "song04"];
+var music = ["song01", "song02", "song03", "song04"];
 
-function iniciar(m){
-	player.iniciar(m);
+function iniciar(m) {
+  player.iniciar(m);
 }
 
 var player = {
-	name: '',
-	age: '',
-	tocar(str) {
-		return console.log(`Tocando ${str}`)
-	}
+  name: "",
+  age: "",
+  tocar(str) {
+    return console.log(`Tocando ${str}`);
+  },
+};
 
+function play() {
+  for (m in music) {
+    console.log(m);
+    player.tocar(m);
+  }
+  console.log("fim");
 }
 
-function play(){
-	for(m in music){
-		console.log(m);
-		player.tocar(m);
-	}
-	console.log("fim");
-}
-function pause(){
-	//sistem.pause();
+function pause() {
+  //sistem.pause();
+  console.log("teste");
 }
 
-function continueMusic(){
-	//sistem.continueMusic();
+function continueMusic() {
+  //sistem.continueMusic();
 }
 
 play();
